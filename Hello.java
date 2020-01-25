337e71885be606d1b022d3858e530316deaa259a
srividhya gopal <srividhya.gopal@gmail.com>
2020-01-25 17:18:38 +0800

    java hello world

---
+++ Hello.java
@@ -0,0 +1,11 @@
+/* First java Program*/
+
+public class Hello {
+
+public static void main (String[] args) {
+System.out.println("Hello world");
+System.out.println(" My new world of programming");
+System.out.println(" this is from text pad");
+}
+}
+
