ca5c92d64ec533a6dfdcdb5e60afa33694490f9d
srividhya gopal <srividhya.gopal@gmail.com>
2020-01-25 17:21:04 +0800

    modified hellow world

--- Hello.java
+++ Hello.java
@@ -6,6 +6,12 @@ public static void main (String[] args) {
 System.out.println("Hello world");
 System.out.println(" My new world of programming");
 System.out.println(" this is from text pad");
+
+for(int i =0 ; i<10;i++)
+{
+System.out.println("Hello world" + i);
+}
+
 }
 }
 
